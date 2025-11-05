abaa8796bc06f4f35469fd3113437a8afb30b8c5
Sergey Korobov <sergeykorobov.frontend@gmail.com>
2025-11-05 15:13:00 +0500

    fix errors

---
+++ .history/src/app/main_20251105145904.tsx
@@ -0,0 +1,13 @@
+//C:\Users\01\training\src\app\main.tsx
+'use client'
+
+import LogIn from './Login/page'
+
+const Main = () => {
+  return (
+    <div className='flex h-screen bg-gray-100'>
+      <LogIn />
+    </div>
+  )
+}
+export default Main
---
+++ .history/src/app/main_20251105150337.tsx
@@ -0,0 +1,14 @@
+//C:\Users\01\training\src\app\main.tsx
+'use client'
+
+import LogIn from './Login/page'
+import LogInPage from './pages/LogIn'
+
+const Main = () => {
+  return (
+    <div className='flex h-screen bg-gray-100'>
+      <LogInPage />
+    </div>
+  )
+}
+export default Main
